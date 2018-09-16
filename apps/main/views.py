3bddfc37a62664b4ad087fd7cf030ef9490101ab
Krishna Patel <kpatel1293@gmail.com>
2018-09-16 01:18:09 -0500

    Initial set up
    Hello World Temporaily put in index page and routed
    
    Created the neccessary floders/files for initial set up

--- apps/main/views.py
+++ apps/main/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
+# index - /
+# render template
+def index(request):
+    return render(request,'index.html')
