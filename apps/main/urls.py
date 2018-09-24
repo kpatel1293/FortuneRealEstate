180e1f4ab95772127162d38c0ea48b7db10b298a
Krishna Patel <kpatel1293@gmail.com>
2018-09-24 02:41:48 -0500

    Completed Backend For - Login and Registration
    -Able to create users
    -Able to login
    -Made the website dynamic with the ablity to also logout
    -created the database
    -added validations for login and registrations

--- apps/main/urls.py
+++ apps/main/urls.py
@@ -2,7 +2,18 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.home, name='home'),                        # home page ('/')
-    url(r'^login^$', views.login, name='login'),                # login page ('/login')
-    url(r'^register^$', views.register, name='register'),       # register page ('/register')
+    # TEMPLATES
+    url(r'^$', views.home, name='home'),                                            # home page         - /
+    url(r'^login$', views.login, name='login'),                                     # login page        - /login
+    url(r'^register$', views.register, name='register'),                            # register page     - /register
+    url(r'^forgotpassword$', views.forgot_pwd, name='forgot_pwd'),                  # forgot password   - /forgotpassword
+    url(r'^dashboard$', views.dashboard, name='dashboard'),                         # dashboard (user)  - /dashboard
+    url(r'^catalog$', views.catalog, name='catalog'),                               # catalog           - /catalog
+    url(r'^contact$', views.contact_us, name='contact_us'),                         # contact us        - /contact
+    url(r'^privacy$', views.privacy, name='privacy'),                               # privacy           - /privacy
+    url(r'^termsofservice$', views.terms_of_service, name='terms_of_service'),      # terms of service  - /termsofservice
+
+    url(r'^user$', views.validatelogin, name='login-user'),                         # logging in users  - /user
+    url(r'^create$', views.create, name='signup'),                                  # register user     - /create
+    url(r'^logout$', views.logout, name='logout')                                   # logout user       - /logout
 ]
--- apps/main/views.py
+++ apps/main/views.py
@@ -1,18 +1,155 @@
 from django.shortcuts import render,redirect
+from django.contrib import messages
+from .models import *
 
 # Create your views here.
 
+# Render Templates
+
 # home - /
-# render template
 def home(request):
-    return render(request,'index.html')
+    check_session = False
+    # check if user in session
+    if 'user_id' in request.session:
+        check_session = True
+
+    context = {
+        'check_session': check_session
+    }
+
+    return render(request,'index.html',context)
 
 # login - /login
-# render template
 def login(request):
-    pass
+    # check if user in session
+    if 'user_id' in request.session:
+        return redirect('main:dashboard')
+
+    return render(request,'login.html')
 
 # registration - /register
-# render template
 def register(request):
-    pass
+    # check if user in session
+    if 'user_id' in request.session:
+        return redirect('main:dashboard')
+
+    return render(request,'register.html')
+
+# forgot password - /forgotpassword
+def forgot_pwd(request):
+    # check if user in session
+    if 'user_id' in request.session:
+        return redirect('main:dashboard')
+
+    return render(request,'forgotpwd.html')
+
+# dashboard (user) - /dashboard
+def dashboard(request):
+    # check if user in session
+    if 'user_id' not in request.session:
+        return redirect('main:home')
+
+    return render(request, 'dashboard.html')
+
+# catalog - /catalog
+def catalog(request):
+    check_session = False
+    # check if user in session
+    if 'user_id' in request.session:
+        check_session = True
+
+    context = {
+        'check_session': check_session
+    }
+
+    return render(request,'catalog.html',context)
+
+# contact us - /contact
+def contact_us(request):
+    check_session = False
+    # check if user in session
+    if 'user_id' in request.session:
+        check_session = True
+
+    context = {
+        'check_session': check_session
+    }
+
+    return render(request,'contactus.html',context)
+
+# privacy - /privacy
+def privacy(request):
+    check_session = False
+    # check if user in session
+    if 'user_id' in request.session:
+        check_session = True
+
+    context = {
+        'check_session': check_session
+    }
+
+    return render(request,'privacy.html',context)
+
+# terms of service - /termsofservice
+def terms_of_service(request):
+    check_session = False
+    # check if user in session
+    if 'user_id' in request.session:
+        check_session = True
+
+    context = {
+        'check_session': check_session
+    }
+
+    return render(request,'terms_of_service.html',context)
+
+# Redirect/Logic
+
+# logging in users - /user
+def validatelogin(request):
+    # validate users
+    valid, result = User.objects.validateLogin(request.POST)
+
+    # check for errors
+    if not valid:
+        for e in result:
+            messages.error(request, e)
+            print e
+        # if there are errors return back to login page 
+        return redirect('main:login')   
+
+    # store/print user in session
+    request.session['user_id'] = result
+    print request.session['user_id']
+    
+    # check for type of user - user/admin/agent
+
+    # redirect to dashboard accordingly
+    return redirect('main:dashboard')
+
+# register user - /create
+def create(request):
+    # validate users
+    valid, result = User.objects.validateRegister(request.POST)
+
+    print valid
+
+    # check for errors
+    if not valid:
+        for e in result:
+            messages.error(request, e)
+        # if there are errors return back to registeration page 
+        return redirect('main:register')
+
+    # store/print user in session
+    request.session['user_id'] = result
+    print request.session['user_id']
+
+    # redirect to user dashboard
+    return redirect('main:dashboard')
+
+# logout user - /logout
+def logout(request):
+    # clear session
+    request.session.clear()
+    return redirect('main:home')
