025a3c5d6cbeea89960d17cf6b68803535c792fa
Krishna Patel <kpatel1293@gmail.com>
2018-09-29 23:14:03 -0500

    added backend for user dashboard

--- apps/main/views.py
+++ apps/main/views.py
@@ -43,7 +43,7 @@ def forgot_pwd(request):
 
     return render(request,'forgotpwd.html')
 
-# dashboard (user) - /dashboard
+# dashboard - /dashboard
 def dashboard(request):
     check_session = False
 
@@ -52,12 +52,40 @@ def dashboard(request):
         return redirect('main:home')
 
     check_session = True
+    show_dash_head = True
+
+    # check user role
+    user_role = User.objects.values('permissionLevel').get(id=request.session['user_id'])['permissionLevel']
+    
+    # get user name
+    user_name = User.objects.values('firstName', 'lastName').get(id=request.session['user_id'])
+    user = '{} {}'.format(user_name['firstName'],user_name['lastName'])
 
     context = {
-        'check_session': check_session
+        'check_session': check_session,
+        'show_head': show_dash_head,
+        'user_role': user_role,
+        'user': user
     }
 
     return render(request, 'dashboard.html',context)
+
+# USER 
+
+# settings - /settings
+
+# AGENT
+
+# listings - /agent/listing
+# create listing - /agent/create
+
+# ADMIN
+
+# users (view all users) - /admin/show
+# create user - /admin/user
+# activity - /admin/activity
+# configure - /admin/config
+
 
 # catalog - /catalog
 def catalog(request):
@@ -78,9 +106,20 @@ def contact_us(request):
     # check if user in session
     if 'user_id' in request.session:
         check_session = True
+        
+    show_dash_head = False
 
+    if check_session:
+        # get user name
+        user_name = User.objects.values('firstName', 'lastName').get(id=request.session['user_id'])
+        user = '{} {}'.format(user_name['firstName'],user_name['lastName'])
+    else:
+        user = ''
+    
     context = {
-        'check_session': check_session
+        'check_session': check_session,
+        'show_head': show_dash_head,
+        'user': user
     }
 
     return render(request,'contactus.html',context)
