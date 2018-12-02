efaf21ac12db49cb4c63753c3451b5a71bca8e6f
Krishna Patel <kpatel1293@gmail.com>
2018-12-02 10:55:22 -0600

    changed ip

--- apps/main/views.py
+++ apps/main/views.py
@@ -848,7 +848,7 @@ def privacy(request):
     else:
         user = ''
 
-    ip_address = '18.191.214.58'
+    ip_address = '13.59.203.157'
 
     context = {
         'check_session': check_session,
@@ -875,7 +875,7 @@ def terms_of_service(request):
     else:
         user = ''
 
-    ip_address = '18.191.214.58'
+    ip_address = '13.59.203.157'
 
     context = {
         'check_session': check_session,
